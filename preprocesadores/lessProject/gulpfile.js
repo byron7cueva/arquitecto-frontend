19f64eb8caa293a173f526b8402703b1ddf8ea17
byron7cueva <byron7cueva@gmail.com>
2019-12-22 10:31:26 -0500

    Creando la estructura del proyecto

---
+++ preprocesadores/lessProject/gulpfile.js
@@ -0,0 +1,33 @@
+const {src, dest, series, watch} = require('gulp');
+const pug = require('gulp-pug');
+const less = require('gulp-less');
+
+/**
+ * Compile pug code
+ */
+function compilePug() {
+  return src('src/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(dest('public/'));
+}
+
+/**
+ * Compile Less
+ */
+function compileLess() {
+  return src('src/less/main.less')
+    .pipe(less())
+    .pipe(dest('public/css/'));
+}
+
+/**
+ * Wacth change files
+ */
+function watchTask() {
+  watch(['src/index.pug', 'views/**/*.pug'], compilePug);
+  watch('src/**/*.less', compileLess);
+}
+
+exports.default = series(compilePug, compileLess, watchTask);
