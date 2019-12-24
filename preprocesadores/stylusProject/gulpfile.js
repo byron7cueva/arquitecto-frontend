bab86d1edb142ce6f6a357256d6cee37530ab9b2
byron7cueva <byron7cueva@gmail.com>
2019-12-24 18:44:44 -0500

    Creando la estructura del proyecto

---
+++ preprocesadores/stylusProject/gulpfile.js
@@ -0,0 +1,38 @@
+const {src, dest, series, watch} = require('gulp');
+const pug = require('gulp-pug');
+const stylus = require('gulp-stylus');
+
+/**
+ * Compile pug code
+ */
+function compilePug() {
+  return src('src/index.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(dest('public/'));
+}
+
+/**
+ * Compile stylus code
+ */
+function compileStylus() {
+  return src('src/stylus/main.styl')
+    .pipe(stylus())
+    .pipe(dest('public/css/'))
+}
+
+/**
+ * Copy images
+ */
+function copyImages() {
+  return src('src/images/*')
+    .pipe(dest('pulic/images/'));
+}
+
+function watchFiles() {
+  watch(['src/index.pug', 'src/pug/**/*.pug'], compilePug);
+  watch('src/stylus/**/*.styl', compileStylus);
+}
+
+exports.default = series(compilePug, compileStylus, watchFiles);
